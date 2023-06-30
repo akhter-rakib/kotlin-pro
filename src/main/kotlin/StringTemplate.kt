a8f3499fd2267a2ce27dd2a9ef82b486f13f0df0
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-06-30 23:22:29 +0600

    String Template

---
+++ src/main/kotlin/StringTemplate.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    var name: String = "Rakib"
+    var age: Int = 35
+    /*Do not use this*/
+    /* var combined = name + age*/
+    /*use the bellow String template for combining values*/
+    var combinedValue = "Name : $name, Age : $age"
+    println(combinedValue)
+}
