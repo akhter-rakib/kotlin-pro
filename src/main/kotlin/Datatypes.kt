670a4eb64e66b02726f0e474238cdeb7a65c728e
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-06-30 19:17:21 +0600

    Datatypes

---
+++ src/main/kotlin/Datatypes.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var n: Int = 10
+    var l: Long = 345L
+    var d: Double = 3.14
+    var f: Float = 12.34F
+    var b: Boolean = true
+    var s: String = "String"
+    var c: Char = 'A'
+    /*Any type can be hold*/
+    var anyType: Any = 123;
+}
