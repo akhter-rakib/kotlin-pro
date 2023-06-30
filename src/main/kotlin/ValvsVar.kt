3998f75ecc7fd81668b4ec9ef6db9062868a649d
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-06-30 19:42:20 +0600

    Var Vs Val

---
+++ src/main/kotlin/ValvsVar.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var varVariable = "Can be changed"
+    varVariable = "changed Value";
+
+    /*final String valVariable = "Not changeable value"*/
+    val valVariable = "Not changeable value"
+    //valVariable ="Can't be changed"
+    println(varVariable)
+    println(valVariable)
+}
