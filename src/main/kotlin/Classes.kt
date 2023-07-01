292b927ccf163285405120b42042b172eacd54b7
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2023-07-01 12:56:05 +0600

    Properties

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -1,10 +1,15 @@
 fun main() {
     /*Object Declaration*/
     val tv = SmartDevice()
+    tv.brand = "Samsung"
+    tv.price = 1234.34
     val ac = SmartDevice()
+    ac.brand = "General"
+    ac.price = 126.45
 }
 
 /*Blue Print*/
 class SmartDevice {
-
+    var brand: String = ""
+    var price: Double = 0.0
 }
